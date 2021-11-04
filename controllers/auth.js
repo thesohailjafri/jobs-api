b7bcfa3a222c130d5e2b9e5cfd7674a18782e57f
thesohailjafri <thesohailjafri@gmail.com>
2021-11-05 01:03:02 +0530

    auth route linked

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,9 @@
 const login = async (req, res) => {
-
+    res.send(req.body)
 }
+
+const register = async (req, res) => {
+    res.send(req.body)
+}
+
+module.exports = { login, register }
--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,5 @@ const { register, login } = require('../controllers/auth')
 router.route('/login').post(login)
 router.route('/register').post(register)
 
-module.exportS = router
+
+module.exports = router
