a336de2eeb136d68a1d0aa6c9844c1c9e3becbd2
thesohailjafri <thesohailjafri@gmail.com>
2021-11-06 00:02:49 +0530

    wuhooo

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use('/api/v1/job', authUser, jobRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 const start = async () => {
   try {
